df97233cc0228c8e6a292e5cd25b329109494c22
Charitha Palagani <charitha.palagani.com>
2026-03-01 13:08:11 +0530

    first commit

---
+++ src/test/java/com/passwordmanager/app/RevPassWordManager2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordmanager.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPassWordManager2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
